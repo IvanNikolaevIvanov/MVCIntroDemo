31cf0124b3e9357daa0f33c8819ac75631203945
bluesbreak <ivanivanovat@gmail.com>
2024-01-21 13:49:28 +0200

    Add project files.

---
+++ MVCIntroDemo/Views/Home/KoStava.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MVCIntroDemo.Views.Home
+{
+    public class KoStavaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
